307ea95923bc2f9d438eacea480e9db6ba8b3cb0
Dicky HK Chu <dicky.hk.chu@prudential.com.hk>
2025-06-05 14:18:10 +0800

    - Adding Package Info to show version no. and build no. - Reconstruct UI to show Version No.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:lottie/lottie.dart';
 
 void main() {
   runApp(const MyApp());
@@ -53,8 +55,61 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
   int _counter = 0;
+  PackageInfo _packageInfo = PackageInfo(
+    appName: 'Unknown',
+    packageName: 'Unknown',
+    version: 'Unknown',
+    buildNumber: 'Unknown',
+    buildSignature: 'Unknown',
+    installerStore: 'Unknown',
+  );
+
+  // Lottie animation controller
+  late AnimationController _controller;
+  bool _showFirework = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initPackageInfo();
+
+    // Init Lottie
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        setState(() {
+          _showFirework = false;
+        });
+        _controller.reset();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> _initPackageInfo() async {
+    final info = await PackageInfo.fromPlatform();
+    setState(() {
+      _packageInfo = info;
+    });
+  }
+
+  Widget _infoTile(String title, String subtitle) {
+    return ListTile(
+      title: Text(title),
+      subtitle: Text(subtitle.isEmpty ? 'Not set' : subtitle),
+    );
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -65,6 +120,23 @@ class _MyHomePageState extends State<MyHomePage> {
       // called again, and so nothing would appear to happen.
       _counter++;
     });
+  }
+
+  void _fireworkAnimation() {
+    // if (_controller.isAnimating) {
+    //   _controller.stop();
+    // } else {
+    //   _controller.repeat();
+    // }
+    // if (_controller.isAnimating) {
+    //   _controller.stop();
+    // } else {
+    //   _controller.repeat();
+    // }
+    setState(() {
+      _showFirework = true;
+    });
+    _controller.forward(from: 0);
   }
 
   @override
@@ -85,37 +157,97 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Stack(
+        children: [
+          Center(
+            // Center is a layout widget. It takes a single child and positions it
+            // in the middle of the parent.
+            child: SingleChildScrollView(
+              child: Column(
+                // Column is also a layout widget. It takes a list of children and
+                // arranges them vertically. By default, it sizes itself to fit its
+                // children horizontally, and tries to be as tall as its parent.
+                //
+                // Column has various properties to control how it sizes itself and
+                // how it positions its children. Here we use mainAxisAlignment to
+                // center the children vertically; the main axis here is the vertical
+                // axis because Columns are vertical (the cross axis would be
+                // horizontal).
+                //
+                // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+                // action in the IDE, or press "p" in the console), to see the
+                // wireframe for each widget.
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text('You have pushed the button this many times:'),
+                  Text(
+                    '$_counter',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                  _infoTile('App name', _packageInfo.appName),
+                  _infoTile('Package name', _packageInfo.packageName),
+                  _infoTile('App version', _packageInfo.version),
+                  _infoTile('Build number', _packageInfo.buildNumber),
+                  _infoTile('Build signature', _packageInfo.buildSignature),
+                  _infoTile(
+                    'Installer store',
+                    _packageInfo.installerStore ?? 'not available',
+                  ),
+                  _infoTile(
+                    'Install time',
+                    _packageInfo.installTime?.toIso8601String() ??
+                        'Install time not available',
+                  ),
+                  _infoTile(
+                    'Update time',
+                    _packageInfo.updateTime?.toIso8601String() ??
+                        'Update time not available',
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+          if (_showFirework)
+            IgnorePointer(
+              ignoring: true, // Allows taps and scrolls to pass through
+              child: Positioned.fill(
+                child: Container(
+                  color: Colors.black.withOpacity(0.2),
+                  child: Center(
+                    child: Lottie.asset(
+                      'assets/firework_animation2.json', // Make sure this file exists in your assets
+                      controller: _controller,
+                      onLoaded: (composition) {
+                        _controller.duration = composition.duration;
+                        _controller.forward(from: 0);
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 16.0),
+            child: FloatingActionButton(
+              onPressed: _incrementCounter,
+              tooltip: 'Increment',
+              child: const Icon(Icons.add),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 16.0),
+            child: FloatingActionButton(
+              onPressed: _fireworkAnimation,
+              tooltip: 'Increment',
+              child: const Icon(Icons.auto_fix_high),
+            ),
+          ),
+        ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
