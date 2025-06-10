72d5c31f2f2a59b512ea419ac1567d1f6f0aedbb
Dicky HK Chu <dicky.hk.chu@prudential.com.hk>
2025-06-10 10:14:32 +0800

    - Change Theme Color (Test Direct Commit Trigger 1014)

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.brown),
       ),
       home: const MyHomePage(title: 'Code Magic Demo'),
     );
